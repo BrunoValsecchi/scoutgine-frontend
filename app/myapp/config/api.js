f2f235c9bd5377dcc9342f10c802005ae173a1c9
Bruno Valsecchi <cai.bruno.valsecchi@gmail.com>
2025-07-01 10:21:10 -0300

    sin docker

--- app/myapp/config/api.js
+++
@@ -1,6 +0,0 @@
-// frontend/app/myapp/config/api.js
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://scoutgine-backend.vercel.app'  // Tu backend en Vercel
-  : 'http://localhost:8000';               // Para desarrollo local
-
-export default API_BASE_URL;
